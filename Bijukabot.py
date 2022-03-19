e4d51216224c0607cb7a792752396280529bb048
Akshita Jain <akshita12252003@gmail.com>
2022-03-20 04:27:15 +0530

    a

--- Bijukabot.py
+++ Bijukabot.py
@@ -6,9 +6,14 @@ Source: https://github.com/python-telegram-bot/python-telegram-bot/blob/master/e
 
 #from curses.panel import update_panels
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import cv2
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, replykeyboardremove
+from telegram.ext import updater, commandhandler, messagehandler, filters, callbackqueryhandler, conversationhandler, callbackcontext
+from telegram.bot import log
+import requests
+x = requests.get("https://hacktoberfestapi.servatom.com/")
+print(x.text)
 import os
-
 
 PORT = int(os.environ.get('PORT', 80))
 
@@ -19,9 +24,12 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 TOKEN = '5171667370:AAFaN1oDzj1qotI-XCsD4kw7ISOGo-HCssE'
 
+latestBackNavigation = InlineKeyboardMarkup([[InlineKeyboardButton( "« Back", callback_data='latest'), InlineKeyboardButton("✗ Exit", callback_data='exit')]])
+trendBackNavigation = InlineKeyboardMarkup([[InlineKeyboardButton( "« Back", callback_data='trend'), InlineKeyboardButton("✗ Exit", callback_data='exit')]])
+
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
+def start(update,       ):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
     
@@ -43,6 +51,12 @@ def main():
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
+
+def image(update, context):
+    update.message.photo[-1].get_file().download("img.jpg")
+    
+    img = cv2.imread()
+
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
