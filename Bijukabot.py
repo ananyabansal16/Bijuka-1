e30b8d1d2532fbf23d2a637585997ca05d01145c
ananyabansal16 <ananya.bansal2003@gmail.com>
2022-03-20 00:25:47 +0530

    did something

--- Bijukabot.py
+++ Bijukabot.py
@@ -58,8 +58,8 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0", port = int(PORT), url_path = TOKEN)
-    updater.bot.setWebhook("https://bijuka.herokuapp.com/" + TOKEN)
+    updater.start_polling()
+    #updater.bot.setWebhook("https://bijuka.herokuapp.com/" + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
